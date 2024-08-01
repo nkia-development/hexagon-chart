86dd25fd3ccc5907a51662a280d6a897c0d9a94a
주강 <jkpark@nkia.co.kr>
2024-08-01 13:57:07 +0900

    Set up TypeScript for HexagonChart
    - Installed ts-loader package
    - Updated build script to tsc && webpack
    - Changed the entry point in webpack.config.js to index.ts
    - Renamed HexagonChart.jsx to HexagonChart.tsx

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
